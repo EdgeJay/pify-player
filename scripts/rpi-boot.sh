c9d3877af3050965c9ece52c76fc823251e10121
Huijie Wu <hjwu85@gmail.com>
2025-03-08 21:03:55 +0800

    fix: update path in rpi-boot.sh for correct repository location

--- scripts/rpi-boot.sh
+++ scripts/rpi-boot.sh
@@ -4,7 +4,7 @@
 # In order to execute script on boot, /etc/rc.local should be modified to include the line:
 # chmod +x /dir_where_script_is_stored/rpi-boot.sh && ./dir_where_script_is_stored/rpi-boot.sh
 
-cd $HOME/repos/pify-player && make start-dev-bg
+cd /home/workbench-rpi-admin/repos/pify-player && make start-dev-bg
 
 until [ "`docker inspect -f {{.State.Running}} pify-player-api`"=="true" ]; do
     sleep 0.5;
