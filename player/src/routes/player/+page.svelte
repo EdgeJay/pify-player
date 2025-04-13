b4b0cd4cdd23de02efc70e8f56a0b104cb73a16b
Huijie Wu <hjwu85@gmail.com>
2025-04-13 23:22:03 +0800

    feat: move logic into YouTube background component

--- player/src/routes/player/+page.svelte
+++ player/src/routes/player/+page.svelte
@@ -5,6 +5,7 @@
 	import { refreshAccessToken } from '$lib/session';
 	import { getAndSaveYoutubeVideo } from '$lib/playback';
 	import PlayerPanel from './components/player-panel.svelte';
+	import YoutubeBackground from './components/youtube-bg.svelte';
 	import LoginDialog from './components/login.svelte';
 
 	const defaultVolume = 50;
@@ -223,19 +224,9 @@
 
 <div class="player-page">
 	<div class="album-bg" style="background-image:url({albumImage})"></div>
-	<div class="video-bg">
-		{#if ytVidId}
-			<iframe
-				width="100%"
-				height="100%"
-				src={`https://www.youtube.com/embed/${ytVidId}?autoplay=1&controls=0&mute=1&loop=1`}
-				title="Background video"
-				frameborder="0"
-				allow="autoplay; clipboard-write; encrypted-media;"
-				style="pointer-events: none;"
-			></iframe>
-		{/if}
-	</div>
+	{#if data.enableYoutube}
+		<YoutubeBackground {ytVidId} />
+	{/if}
 	<PlayerPanel
 		{player}
 		{errorMessage}
@@ -271,14 +262,5 @@
 		z-index: 0;
 		opacity: 0.5;
 		filter: blur(5px);
-	}
-
-	.video-bg {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		z-index: 1;
 	}
 </style>
---
+++ player/src/routes/player/components/youtube-bg.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	interface Props {
+		ytVidId?: string;
+	}
+
+	let { ytVidId }: Props = $props();
+</script>
+
+<div class="video-bg">
+	{#if ytVidId}
+		<iframe
+			width="100%"
+			height="100%"
+			src={`https://www.youtube.com/embed/${ytVidId}?autoplay=1&controls=0&mute=1&loop=1`}
+			title="Background video"
+			frameborder="0"
+			allow="autoplay; clipboard-write; encrypted-media;"
+			style="pointer-events: none;"
+		></iframe>
+	{/if}
+</div>
+
+<style>
+	.video-bg {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		z-index: 1;
+	}
+</style>
